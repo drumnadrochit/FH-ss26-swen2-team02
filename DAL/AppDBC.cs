d53495448ebd28a4ca54fc9d9b161c6c06def46a
jaspher <if24b110@technikum-wien.at>
2026-06-16 15:46:00 +0200

    Changes DbContext * DbSet TourLog added

--- DAL/AppDBC.cs
+++ DAL/AppDBC.cs
@@ -7,6 +7,7 @@ public class AppDBC : DbContext
 {
     public DbSet<Tour> Tours { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<TourLog> TourLogs { get; set; } 
 
 
     public AppDBC(DbContextOptions<AppDBC> options) : base(options)
