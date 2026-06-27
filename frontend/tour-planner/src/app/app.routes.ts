3969f0e8942b79b957628f5a4db15f2bb46090be
Bozo Vuksan <bozovuksan@gmail.com>
2026-06-27 12:03:43 +0200

    Adds HttpClient Service Adds Routes (Login, Register)

--- frontend/tour-planner/src/app/app.routes.ts
+++ frontend/tour-planner/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {Login} from './views/login/login';
+import {Register} from './views/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Login},
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  }
+];
