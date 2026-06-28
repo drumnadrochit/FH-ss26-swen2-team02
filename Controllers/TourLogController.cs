3c05ed0c9e81f6c63a652b0c2eb281df26c02737
Jaspher <if24b110@technikum-wien.at>
2026-06-28 15:15:31 +0200

    Adds Tourlog Controller

---
+++ Controllers/TourLogController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TourPlanner.API.DTO;
+using TourPlanner.Services;
+
+namespace TourPlanner.Controllers
+{
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TourLogController : ControllerBase
+    {
+        private readonly TourLogService _tourLogService;
+
+        public TourLogController(TourLogService service)
+        {
+            _tourLogService = service;
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult> CreateTourLog([FromBody] TourLogDTO tourLogDto)
+        {
+            try
+            {
+                var tl = await _tourLogService.AddTourLog(tourLogDto);
+                return Ok(tl);
+            }
+            catch (Exception ex)
+            {
+                // Sauber: Nur die Nachricht als JSON-Objekt zurückgeben
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<ActionResult> UpdateTourLog([FromBody] TourLogDTO tourLog, int id)
+        {
+            try
+            {
+                var t = await _tourLogService.UpdateTour(tourLog, id);
+                return Ok(t);
+            }
+            catch (Exception ex)
+            {
+                return Conflict(new { error = ex.Message });
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult> DeleteTourLog(int id, [FromBody] TourLogDeleteDTO tourId)
+        {
+            try
+            {
+                await _tourLogService.DeleteTourLog(id, tourId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Sauber: Nur die Nachricht als JSON-Objekt zurückgeben
+                return Conflict(new { error = ex.Message });
+            }
+        }
+        
+        
+        
+    }
+}
